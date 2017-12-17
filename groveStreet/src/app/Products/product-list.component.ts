2f8eb4f83605d79c7ab49651cfa4e89ab8545c9a
superflowr <kralj.vika@gmail.com>
2017-12-17 18:04:08 +0100

    Merge remote-tracking branch 'origin/Category-component' into Category-component
    # Conflicts:
    #	.idea/workspace.xml

--- groveStreet/src/app/Products/product-list.component.ts
+++ groveStreet/src/app/Products/product-list.component.ts
@@ -5,5 +5,34 @@ import { Component } from '@angular/core' ;
   templateUrl: './product-list.component.html'
 })
 export class ProductListComponent {
+  pageTitle = 'Product List';
+  imageWidth = 100;
+  imageMargin = 2;
+  showImage = false;
+  products: any [] = [
+    {
+      'productId': 1,
+      'productName': 'Hamburger',
+      'productCode': 'GDN-0011',
+      'releaseDate': 'March 19, 2016',
+      'description': 'Slovenian home made Hamburgers.',
+      'price': 5.95,
+      'starRating': 5.0,
+      'imageUrl': 'https://c.tadst.com/gfx/750x500/hamburger-day.jpg?1'
+    },
+    {
+      'productId': 2,
+      'productName': 'PIZZA',
+      'productCode': 'GDN-0023',
+      'releaseDate': 'March 18, 2016',
+      'description': 'Best pizzas ever made.',
+      'price': 9.99,
+      'starRating': 5.0,
+      'imageUrl': 'http://livelonglivegreek.com/wp-content/uploads/2017/05/Pizza-margarita.png'
+    },
+    ];
 
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
 }
